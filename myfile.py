35134e16b1ef6770e432b33bcbd96b876f25c657
Abhivarmas <abhishekvarmas98@yahoo.com>
2022-07-10 09:41:12 +0530

    changes done

--- myfile.py
+++ myfile.py
@@ -2,3 +2,8 @@ print("hello i am abhishek this is lab test code:")
 a=[1,2,3,4,5,6,7,8,9]
 b=min(a)
 print("minimum number of list answer=",b)
+
+
+c=max(a)
+print("minimum number of list answer=",c)
+
